88247dfa9cd2128a0d232a6a686803c7f4059872
NeutrinoBlue <70873800+NeutrinoBlue@users.noreply.github.com>
2024-07-09 22:07:13 -0600

    Add files via upload
    More

--- setup_system.sh
+++ setup_system.sh
@@ -9,13 +9,23 @@ sudo dnf update -y
 # Install desired packages
 sudo dnf install -y discord polybar rofi vlc neovim kcolorchooser kitty flameshot timeshift
 
+# Install Python (if not already installed)
+sudo dnf install -y python3
+
+# Install pip (if not already installed)
+sudo dnf install -y python3-pip
+
+# Install pywal using pip
+sudo pip3 install pywal
+
 # Set hostname to "field"
 sudo hostnamectl set-hostname "field"
-
-# Copy .config folder from flash drive to home directory (adjust path accordingly)
 
 rsync -av /run/media/strwbry/2247-2752/strwbry/.config/polybar/ ~/.config/polybar/
 rsync -av /run/media/strwbry/2247-2752/strwbry/.config/rofi/ ~/.config/rofi/
 rsync -av /run/media/strwbry/2247-2752/strwbry/.config/picom/ ~/.config/picom/
+rsync -av /run/media/strwbry/2247-2752/strwbry/.config/i3/ ~/.config/i3/
+rsync -av /run/media/strwbry/2247-2752/strwbry/.config/neofetch/ ~/.config/nitrogen/
 
 echo "Setup completed."
+
